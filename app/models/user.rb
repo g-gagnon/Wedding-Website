cedc36ae06182cf4ad41d954c9ef8482f64a2b43
Gabriel Gagnon <gabriel.gagnon@smartika.com>
2017-11-11 16:02:08 -0500

    Getting closer to release
    Former-commit-id: 8084a515d984539270d900d09fb02f63067ed921

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
 
+  has_one :user_config, dependent: :destroy
+  has_one :rsvp, dependent: :destroy
+
+  before_create :build_default_user_config
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -12,5 +17,10 @@ class User < ApplicationRecord
       user.name = auth.info.name
       user.image = auth.info.image
     end
+  end
+
+  def build_default_user_config
+    build_user_config(invitation_type: 'NONE')
+    true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,29 @@
 Rails.application.routes.draw do
 
-  root 'pages#index'
-
-  devise_scope :user do
-    get "/sign_in" => "devise/sessions#new"
-    get "/sign_out" => "devise/sessions#destroy"
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
-  end
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  get '/blog', to: 'pages#blog'
-  get '/blog-single', to: 'pages#blog'
-  get '/bridesmaids', to: 'pages#bridgemails'
-  get '/contacts', to: 'pages#contacts'
-  get '/page', to: 'pages#page'
-  get '/photos', to: 'pages#photos'
-  get '/rsvp', to: 'pages#rsvp'
-  get '/venue', to: 'pages#venue'
+  scope "(:locale)", locale: /en|fr/ do
+    devise_scope :user do
+      get "/sign_in" => "devise/sessions#new"
+      get "/sign_out" => "devise/sessions#destroy"
+      get "/sign_up" => "devise/registrations#new"
+    end
 
+    get '/blog', to: 'pages#blog'
+    get '/blog-single', to: 'pages#blog'
+    get '/bridesmaids', to: 'pages#bridesmaids'
+    get '/contact', to: 'pages#contact'
+    get '/page', to: 'pages#page'
+    get '/photos', to: 'pages#photos'
+    get '/venue', to: 'pages#venue'
+
+    get '/rsvp', to: 'rsvp#rsvp'
+    match '/code', to: 'rsvp#code', via: [:get, :post]
+    resource :rsvp, controller: 'rsvp'
+
+
+    get '/:locale' => 'pages#index'
+    root 'pages#index'
+  end
 
 end
