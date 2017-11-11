294502a02d781a1835697967d11a9cdd9416f1a2
Gabriel Gagnon <gabriel.gagnon@smartika.com>
2017-11-11 16:02:08 -0500

    Getting closer to release
    Former-commit-id: ce123e1c861e60729bdbf0927e1a4411c74d8219

---
+++ app/models/rsvp.rb
@@ -0,0 +1,9 @@
+class Rsvp < ApplicationRecord
+  self.table_name= 'rsvp'
+
+  belongs_to :user
+  has_many :rsvp_guests, inverse_of: :rsvp, dependent: :destroy
+  accepts_nested_attributes_for :rsvp_guests, allow_destroy: true
+
+
+end
